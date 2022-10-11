65c96564a04db04faed340f507a0e7cae1ad6159
Gor Beglaryan <armgor@gmail.com>
2022-10-10 20:17:45 -0700

    added ability to read .env content

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,19 @@
 var path = require('path')
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const mockAPIResponse = require('./mockAPI.js')
-
 const app = express()
+
+const dotenv = require('dotenv').config({path: path.join(__dirname, '../../.env')});
+const parsed_env = dotenv.parsed;
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+console.log(parsed_env.GEONAMES_USERNAME)
+
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
